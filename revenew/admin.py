7f09b883390019294baa29fb30571decbf61bbbd
MikeRophone64 <mm.64studioz@gmail.com>
2020-12-27 10:30:04 +0100

    Created Hotel Admin ListView

--- revenew/admin.py
+++ revenew/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
-admin.site.register(models.Hotels)
+
+admin.site.register(models.HotelCategory)
+
+
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbrev', 'stars', 'category')
+    ordering = ['-stars']
+
+admin.site.register(models.Hotels, HotelAdmin)
--- revenew/models.py
+++ revenew/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class HotelCategory(models.Model):
+    name = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
 class Hotels(models.Model):
-    category_choices = [
-        ("Disney Hotel", "Disney Hotel"),
-        ("Partner Hotel", "Partner Hotel")
-    ]
     name = models.CharField(max_length=64)
     abbrev = models.CharField(max_length=16)
     stars = models.IntegerField()
-    category = models.CharField(max_length=32, choices=category_choices, default=category_choices[0])
+    category = models.ForeignKey(HotelCategory, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
