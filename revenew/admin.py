39dd437fa951f59f658dedb667d1080eb9b12224
MikeRophone64 <mm.64studioz@gmail.com>
2020-12-27 12:18:38 +0100

    updated Admin Site: can change values from list view

--- revenew/admin.py
+++ revenew/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from . import models
 
+
+admin.site.site_header = "ReveNew Dashboard"
 admin.site.register(models.HotelCategory)
 
-
+@admin.register(models.Hotels)
 class HotelAdmin(admin.ModelAdmin):
     list_display = ('name', 'abbrev', 'stars', 'category')
     ordering = ['-stars']
 
-admin.site.register(models.Hotels, HotelAdmin)
+@admin.register(models.CityTax)
+class CityTaxAdmin(admin.ModelAdmin):
+    list_display = ('hotel', 'EUR', 'GBP', 'USD', 'CHF', 'DKK')
+    list_editable = ('EUR', 'GBP', 'USD', 'CHF', 'DKK',)
+    ordering = ['-hotel__stars']
+    list_filter = ['hotel__category']
--- revenew/models.py
+++ revenew/models.py
@@ -2,16 +2,41 @@ from django.db import models
 
 # Create your models here.
 class HotelCategory(models.Model):
+
+    class Meta:
+        verbose_name = "Hotel Category"
+        verbose_name_plural = "Hotel Categories"
+
     name = models.CharField(max_length=32)
 
     def __str__(self):
         return self.name
 
 class Hotels(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Hotels"
+    
     name = models.CharField(max_length=64)
-    abbrev = models.CharField(max_length=16)
+    abbrev = models.CharField(max_length=16, verbose_name="Abbreviation")
     stars = models.IntegerField()
     category = models.ForeignKey(HotelCategory, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
+
+
+class CityTax(models.Model):
+
+    class Meta:
+        verbose_name = "City Tax"
+        verbose_name_plural = "City Taxes"
+    hotel = models.OneToOneField(Hotels, on_delete=models.CASCADE)
+    EUR = models.DecimalField(max_digits=5, decimal_places=2)
+    GBP = models.DecimalField(max_digits=5, decimal_places=2)
+    USD = models.DecimalField(max_digits=5, decimal_places=2)
+    CHF = models.DecimalField(max_digits=5, decimal_places=2)
+    DKK = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return "Tax:" + self.hotel.name
