c28d9ff4c68e03904c3f756d2423519cd0fcd9e7
MikeRophone64 <mm.64studioz@gmail.com>
2020-12-30 23:01:03 +0100

    added input fields and datepicker to sidebar

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,9 +1,8 @@
 import React, { Component, useState } from 'react';
 // import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
-import AccessAlarmOutlinedIcon from '@material-ui/icons/AccessAlarmOutlined';
-import { Button } from '@material-ui/core';
-import Test from './texting';
-
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import MyDate from './datepicker';
 
 export default class Sidebar extends Component {
     constructor(props) {
@@ -17,24 +16,74 @@ export default class Sidebar extends Component {
         this.setState({ text: myText });
     }
 
+    checkPartyMix() {
+        const adults = document.getElementById("num_adults");
+        const teens = document.getElementById("num_teens");
+        const children = document.getElementById("num_children");
 
+        // Check adults
+        if (adults.value > 6) {
+            adults.value = 6;
+        } else if (adults.value < 0) {
+            adults.value = 0;
+        }
+
+        // Check teens
+        if (teens.value > 6) {
+            teens.value = 6;
+        } else if (teens.value < 0) {
+            teens.value = 0;
+        }
+
+        // Check children
+        if (children.value > 6) {
+            children.value = 6;
+        } else if (children.value < 0) {
+            children.value = 0;
+        }
+
+
+    }
     
     render() {
 
         return (
             <nav className="col-md-2 d-none d-md-block bg-light sidebar">
                 <div className="sidebar-sticky">
-                    <ul className="nav flex-column">
-                        <li className="nav-item" >Hello</li>
-                        <li className="nav-item" >World</li>
-                        <li className="nav-item" ><AccessAlarmOutlinedIcon /></li>
-                        <li className="nav-item" onClick={this.changeText}>{this.state.text}</li>
-                        <Button onClick={this.changeText}>
-                            {this.state.text}
-                        </Button>
-                        <Test />
-
-                    </ul>
+                    <div>
+                        <MyDate />
+                        <TextField
+                            onChange={this.checkPartyMix}
+                            id="num_adults"
+                            label="Adults"
+                            type="number"
+                            // Add Mix Max later (messes with CSS)
+                            // InputProps={{ 
+                            //     inputProps: {
+                            //         min:0,
+                            //         max:6
+                            //     }
+                            // }}
+                            defaultValue="1"
+                        />
+                        <TextField
+                            onChange={this.checkPartyMix}
+                            id="num_teens"
+                            label="Teens"
+                            type="number"
+                        />
+                        <TextField
+                            onChange={this.checkPartyMix}
+                            id="num_children"
+                            label="Children"
+                            type="number"
+                        />
+                        <TextField
+                            id="room_price"
+                            label="Room Price"
+                            type="number"
+                        />
+                    </div>
                 </div>
             </nav>
             
---
+++ frontend/src/components/datepicker.js
@@ -0,0 +1,40 @@
+import 'date-fns';
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
+export default function MyDate() {
+  // The first commit of Material-UI
+  const now = Date.now()
+  const [selectedDate, setSelectedDate] = React.useState(new Date(now));
+  console.log(selectedDate)
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <Grid container justify="space-around">
+        <KeyboardDatePicker
+          disableToolbar
+          variant="inline"
+          format="dd/MM/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Date of Arrival"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change date',
+          }}
+        />
+      </Grid>
+    </MuiPickersUtilsProvider>
+  );
+}
