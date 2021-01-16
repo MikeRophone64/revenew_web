b7cebe349f05782440704aec71d1b5195c68169c
MikeRophone64 <mm.64studioz@gmail.com>
2021-01-16 23:20:08 +0100

    created redux middleware

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -1,8 +1,14 @@
 
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import reducer from './reducer';
-
+import logger from './middleware/logger';
 
 export default function() {
-    return configureStore({ reducer });
+    return configureStore({ 
+        reducer,
+        middleware: [
+            ...getDefaultMiddleware(),
+            logger
+        ]
+    });
 }
