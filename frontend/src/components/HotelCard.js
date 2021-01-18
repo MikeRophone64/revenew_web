f77b5d6195672f045fa3d818df7c4b0eedac7399
MikeRophone64 <mm.64studioz@gmail.com>
2021-01-18 11:28:32 +0100

    hotels updated in store on click

--- frontend/src/components/HotelCard.js
+++ frontend/src/components/HotelCard.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-
+import StoreContext from '../contexts/storeContexts';
+import { hotelChanged } from '../store/hotels';
 
 export default function HotelCard(props) {
+
+  const store = useContext(StoreContext);
+  const [activeClass, setActiveClass] = useState(false)
+
+  const handleClick = (hotel) => {
+    store.dispatch(hotelChanged({ selectedHotel: hotel }))
+  }
 
   return (
     <Grid item lg={4} md={6} xs={12}>
       <Card 
-        onClick={() => props.handleClick(props.hotel.name)}
+        onClick={() => handleClick(props.hotel.abbrev)}
       >
         <CardActionArea>
           <CardMedia
